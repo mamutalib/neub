5348bb635ad7a953dffedab0029cc302592ba24b
mamutalib <mohammadmutalib678@gmail.com>
2022-08-01 16:26:38 +0600

    initial commit

---
+++ Dual semester 4-1/Artificial-Intellegence/Artificial-Intellegence-Lab/Practice-lab-2/pythonNumber.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  1 16:01:34 2022
+
+@author: MUTALiB
+"""
+
+x = 100001111111110234
+y = 2234j
+z = -10e10 # e is used write the scientific or big number in shortform e = 10
+
+
+print(x)
+print(y)
+print(z)
+
+
+
+
---
+++ Dual semester 4-1/Artificial-Intellegence/Artificial-Intellegence-Lab/Practice-lab-2/variable.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  1 15:16:53 2022
+
+@author: MUTALiB
+"""
+
+x = 2
+y = 3
+
+print(x+y)
+
+a = int(10)
+b = str(20)
+c = float(3)
+
+## 
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+## Python varible are case-sentitive; 'A' and 'a' are different vaiable. 
+
+# Multi Varible in one line
+x,y,z = "hello",3,4
+
+print(x)
+print(y)
+print(z)
+
+# printing in one line
+
+print(x,y,z)
