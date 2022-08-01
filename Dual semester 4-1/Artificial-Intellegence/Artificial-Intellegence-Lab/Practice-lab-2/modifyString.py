cfcc657d80356f00cefb1ba2d025181c88b23359
mamutalib <mohammadmutalib678@gmail.com>
2022-08-01 18:39:02 +0600

    initial commit

--- Dual semester 4-1/Artificial-Intellegence/Artificial-Intellegence-Lab/Practice-lab-2/modifyString.py
+++ Dual semester 4-1/Artificial-Intellegence/Artificial-Intellegence-Lab/Practice-lab-2/modifyString.py
@@ -19,3 +19,29 @@ print(s3) # It's remove ' ' white space from the beging and ending
 
 s4 = s.replace('string', 'StRinG')
 print(s4)
+
+
+s = "hey, python"
+
+print(s.capitalize())
+print(s.casefold())
+print(s.count('e',0,10))
+print(s.encode())
+
+
+
+
+print(s1 + s2 + s3)
+
+
+
+myName = "Mr. Frankling"
+myAge = 234
+
+print("My name is: "+"My age is: "+format(myAge))
+
+
+
+
+
+
