22ac31532837f4d2c2ac50625ef6c79900d79e95
mamutalib <mohammadmutalib678@gmail.com>
2022-08-12 15:32:11 +0600

    new files added

---
+++ Dual semester 4-1/Artificial-Intellegence/Artificial-Intellegence-Lab/Practice-lab-2/dic.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 15:26:24 2022
+
+@author: Abdul Mutalib
+"""
+
+data = {
+        "name" : "Md. Abdul Mutalib",
+        "age" : 24,
+        "ID" : 190303020001,
+        "Session": "Fal'19"
+        
+        }
+
+print(data)
