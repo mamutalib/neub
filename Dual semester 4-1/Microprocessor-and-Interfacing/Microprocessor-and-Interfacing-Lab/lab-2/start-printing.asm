b104a7b156d5199808448b23f96bff38c4b75b1c
mamutalib <mohammadmutalib678@gmail.com>
2022-08-17 01:07:52 +0600

    updated

--- Dual semester 4-1/Microprocessor-and-Interfacing/Microprocessor-and-Interfacing-Lab/lab-2/start-printing.asm
+++ Dual semester 4-1/Microprocessor-and-Interfacing/Microprocessor-and-Interfacing-Lab/lab-2/start-printing.asm
@@ -1,36 +1,63 @@
+
 .MODEL SMALL
 .STACK 100H
-.DATA   
-    msg db '**********', 0dh, 0ah, '$'  
-
+.DATA  
+    msg db '**********', 0dh, 0ah, '$'
 .CODE
-
-MAIN PROC   
-    mov ax,@data
+MAIN PROC                             
+    
+    mov ax, @data
     mov ds, ax
     
-    mov ah, 9         ; string print
-    lea dx, msg        ; starting point
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-    int 21h
-      
+    mov ah, 9
+    lea dx, msg
     
- 
-   
+    int 21h
+    int 21h
+    int 21h
+    int 21h
+    int 21h
+    int 21h
+    int 21h
+    int 21h
+    int 21h
+    int 21h
 
-    MOV AH, 4CH
-    INT 21H
-
-MAIN ENDP
+    mov ah, 4ch
+    int 21h
+    MAIN ENDP
 END MAIN
-
+;.MODEL SMALL
+;.STACK 100H
+;.DATA   
+;    msg db '**********', 0dh, 0ah, '$'  
+;
+;.CODE
+;
+;MAIN PROC   
+;    mov ax,@data
+;    mov ds, ax
+;    
+;    mov ah, 9         
+;    lea dx, msg       
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;    int 21h
+;      
+;
+;    MOV AH, 4CH
+;    INT 21H
+;
+;MAIN ENDP
+;END MAIN
+;
+;
