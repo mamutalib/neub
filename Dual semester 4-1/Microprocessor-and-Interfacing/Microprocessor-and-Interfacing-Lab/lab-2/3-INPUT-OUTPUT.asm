69709ed7bdbdc35781e65aa36fd3fc622b9c6343
mamutalib <mohammadmutalib678@gmail.com>
2022-08-17 00:36:13 +0600

    updated

--- Dual semester 4-1/Microprocessor-and-Interfacing/Microprocessor-and-Interfacing-Lab/lab-2/3-INPUT-OUTPUT.asm
+++ Dual semester 4-1/Microprocessor-and-Interfacing/Microprocessor-and-Interfacing-Lab/lab-2/3-INPUT-OUTPUT.asm
@@ -1,65 +1,63 @@
-.MODEL SMALL
+.MODEL
 .STACK 100H
-.DATA   
-    msg db 'Enter three  character: $'     
-
+.DATA  
+    msg db 'Enter three character: $'
     crlf db 0dh, 0ah, '$'
 .CODE
-
-MAIN PROC   
-    mov ax,@data
+MAIN PROC 
+    mov ax, @data
     mov ds, ax
     
-    mov ah, 9         ; string print
-    lea dx, msg        ; starting point
-    int 21h                         
-           
+    mov ah, 9
+    lea dx, msg
+    int 21h
+    
     mov ah, 1
-    int 21h
-    mov bl, al
-    int 21h
     
-    mov bh,al
     int 21h
-    
+    mov bl, al 
+    int 21h
+    mov bh, al
+    int 21h
     mov cl, al
-     
-     
-    mov ah,2     ;print 1ST CHARACTER
-    mov dl, 0dh
-    int 21h
-    
-    mov dl, 0ah ;PRINT NEW LINE
-    int 21h
-    mov dl, bl
-    int 21h    
-    
-    mov ah,2     ;print 2ND CHARACTER
+
+    mov ah,2
     mov dl, 0dh
     int 21h
     mov dl, 0ah
     int 21h
-
+     
+    mov dl, bl
+    int 21h  
+    
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h
+     
+    
+    
     mov dl, bh
     int 21h   
     
-    mov ah,2     ;print 3RD CHARACTER
     mov dl, 0dh
     int 21h
-    
     mov dl, 0ah
     int 21h
-
+     
+    
     mov dl, cl
+    int 21h  
+    
+    
+    mov dl, 0dh
     int 21h
+    mov dl, 0ah
+    int 21h
+     
     
-    
-    
-    
-
-    MOV AH, 4CH
+    MOV AH, 4CH  
     INT 21H
-
-MAIN ENDP
+    MAIN ENDP
 END MAIN
 
