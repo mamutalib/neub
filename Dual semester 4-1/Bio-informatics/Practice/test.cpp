0b44ecab696ec9c4720d5a25b96888e59c8b369c
mamutalib <mohammadmutalib678@gmail.com>
2022-09-07 02:02:15 +0600

    big updated

--- Dual semester 4-1/Bio-informatics/Practice/test.cpp
+++ Dual semester 4-1/Bio-informatics/Practice/test.cpp
@@ -128,10 +128,158 @@ int main(void) {
 } */
 
 // RNA Reverse 
+/* #include <bits/stdc++.h>
+using namespace std;
+
+void rnaReverse(string str) {
+    int len = str.size();
+    
+    for (int i = len; i>=0; i--) {
+        if (str[i] == 'A') str[i] = 'U';
+        else if (str[i] == 'U') str[i] = 'A';
+        else if (str[i] == 'C') str[i] = 'G';
+        else if (str[i] == 'G') str[i] = 'C';
+    }
+    for(int i = len; i>=0; i--) {
+        cout << str[i];
+    }
+}
+
+int main(void) {
+    string str;
+    cin >> str;
+
+    rnaReverse(str);
+    return 0;
+} */
+
+//Smit Waterman Algorithm
+/* #include <bits/stdc++.h>
+using namespace std;
+
+void smitWatermanAlgorithm(string ls, string rs) {
+    int row, col;
+    row = ls.size()+1;
+    col = rs.size()+1;
+
+    int data[row][col];
+    
+    //initialization with zero 
+    for(int i = 0; i<row; i++) {
+        for (int j = 0; j<col; j++) {
+            data[i][j] = 0;
+        }
+    }
+    
+    int match = 1, misMatch= -1, gap = -2;
+    int left_adj = 0, up_adj = 0, dia_adj = 0;
+
+    for (int i =1; i<row; i++) {
+        for (int j = 1; j<col; j++) {
+            left_adj = data[i-1][j] + gap;
+            if(left_adj < 0) {
+                left_adj = 0;
+            }
+            up_adj = data[i][j-1] + gap;
+            if(up_adj < 0) {
+                up_adj = 0;
+            }
+
+            if (ls[i-1] == rs[j-1]) {
+                dia_adj = data[i-1][j-1] + match;
+            } 
+            else {
+                dia_adj = data[i-1][j-1] + misMatch;
+            }
+
+            data[i][j] = max(left_adj, up_adj);
+            data[i][j] = max(data[i][j], dia_adj);
+            
+        }
+    }
+
+    for (int i = 0; i<row; i++) {
+        for (int j = 0; j<col; j++) {
+            cout << data[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(void) {
+    string ls,rs;
+    cin >> ls >> rs;
+
+    smitWatermanAlgorithm(ls,rs);
+    return 0;
+} */
+
+
+//Needleman Algorithm
 #include <bits/stdc++.h>
 using namespace std;
 
+void needlemanAlgo(string ls, string rs) {
+    int row, col;
+    row = ls.size()+1;
+    col = rs.size()+1;
+
+    int data[row][col];
+
+    for(int i = 0; i<row; i++)
+        for (int j = 0; j<col; j++)
+            data[i][j] = 0;
+    
+    for (int i = 0; i<1; i++) 
+        for (int j = 1; j<col; j++)
+            data[i][j] = -j*2;
+        
+    
+    for (int i = 0; i<row; i++) 
+        for (int j = 0; j<1; j++)
+            data[i][j] = -i*2;
+
+
+    for (int i = 0; i<row; i++) 
+        for (int j = 0; j<col; j++)
+            cout << data[i][j];
+
+        cout << endl;
+    
+    int match = 1 ,misMatch = -1,gap = -2;
+    int lef_adj = 0, up_adj = 0, dia_adj = 0;
+
+
+    for (int i = 1; i<row; i++) {
+        for(int j = 1; j<row; j++) {
+
+            lef_adj = data[i-1][j] + gap;
+            up_adj = data[i][j-1] + gap;
+
+            if (ls[i-1] == rs[j-1])
+                dia_adj = data[i-1][j-1] + match;
+            else 
+                dia_adj = data[i-1][j-1] + misMatch;
+            
+            data[i][j] = max(lef_adj, up_adj);
+            data[i][j] = max(data[i][j], dia_adj);
+        }
+    }
+    cout << endl;
+
+    for (int i =0; i<row; i++) 
+        for (int j =  0; j<col; j++) 
+            cout << data[i][j] << " ";
+        
+        cout << endl;
+    
+}
+
 int main(void) {
+    string ls, rs;
+    cin >> ls >> rs;
+
+    needlemanAlgo(ls,rs);
     
     return 0;
 }
